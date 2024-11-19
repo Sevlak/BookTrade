1288a1bec1b8c2d8ae862a44d891f175d9847017
William Alves <willoandrade29@gmail.com>
2024-11-19 10:34:53 -0300

    refactor: move o offcanvas para baixo

--- BookTrade.Web/wwwroot/js/site.js
+++ BookTrade.Web/wwwroot/js/site.js
@@ -2,15 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-document.addEventListener("mousemove", (event) => {
-    const screenWidth = window.innerWidth;
-    const threshold = 400;
-    const offcanvas = bootstrap.Offcanvas.getOrCreateInstance(document.getElementById('offcanvasExample'))
-
-    if (screenWidth - event.clientX < threshold) {
-        offcanvas.show();
-    } else {
-        offcanvas.hide();
-    }
-})
