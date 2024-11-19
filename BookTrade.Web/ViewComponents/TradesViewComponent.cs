2c8424e70f539c3004702ba53453165a85b1a84d
William Alves <willoandrade29@gmail.com>
2024-11-19 00:24:51 -0300

    feat: adiciona componente offcanvas para exibir trades
    Modifica "site.js" para ativar/desativar o offcanvas com base na posição do mouse.

---
+++ BookTrade.Web/ViewComponents/TradesViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookTrade.Web.ViewComponents;
+
+public class TradesViewComponent: ViewComponent
+{
+    //TODO: We should gather all ongoing trades for the user here
+    public TradesViewComponent()
+    {
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var test = new List<string> { "Test1", "Test2", "Test3" };
+        return View(test);
+    }
+}
