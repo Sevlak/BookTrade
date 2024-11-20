48d7f804696ad23d8eb178c6446d2993108596ca
William Alves <willoandrade29@gmail.com>
2024-11-20 15:42:32 -0300

    feat: adiciona mapa

---
+++ BookTrade.Web/wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿const map = L.map('map', {
+    center: [-8.063, -34.871],
+    zoom: 17
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
